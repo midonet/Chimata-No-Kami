4684fdf3273c86d46de3d8ee2d2013322a2018d4
Alexander Gabert <alexander.gabert@gmail.com>
2015-08-15 19:25:08 +0200

    ssh config for servers

--- stages/sshconfig/fabfile.py
+++ stages/sshconfig/fabfile.py
@@ -1,1 +1,37 @@
     puts(green("creating local ssh config for %s" % env.host_string))
+
+    local("""
+TMPDIR="%s"
+HOSTNAME="%s"
+DOMAIN="%s"
+IP="%s"
+
+cat >"${TMPDIR}/.ssh/.config.fragment.${HOSTNAME}.txt" <<EOF
+#
+# ssh config for server ${HOSTNAME} (${IP})
+#
+Host ${HOSTNAME}
+    User root
+    ServerAliveInterval 2
+    KeepAlive yes
+    ConnectTimeout 30
+    TCPKeepAlive yes
+    Hostname ${IP}
+
+Host ${HOSTNAME}.${DOMAIN}
+    User root
+    ServerAliveInterval 2
+    KeepAlive yes
+    ConnectTimeout 30
+    TCPKeepAlive yes
+    Hostname ${IP}
+
+EOF
+
+""" % (
+        os.environ["TMPDIR"],
+        env.host_string,
+        metadata.config['domain'],
+        metadata.servers[env.host_string]['ip']
+    ))
+
