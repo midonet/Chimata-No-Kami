87619430a9fb370f6e9928284b91147ee9c285dc
Alexander Gabert <alexander.gabert@gmail.com>
2015-08-15 20:56:07 +0200

    install target and puppet helper class

---
+++ lib/chimata/utils.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python -Werror
+
+#
+# Copyright (c) 2015 Midokura SARL, All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import os
+
+from fabric.api import *
+
+class Daemon(object):
+    def __init__(self, configfile):
+        pass
+
+    @classmethod
+    def poll(cls, daemon_string, timeout=15):
+        run("""
+DAEMON_STRING="%s"
+TIMEOUT="%i"
+
+RETURNCODE=1
+
+for i in $(seq 1 "${TIMEOUT}"); do
+    ALIVE="$(ps axufwwwwwwwwwwwwwwwwwwwwww | grep -v grep | grep "${DAEMON_STRING}")"
+
+    if [[ "" == "${ALIVE}" ]]; then
+        RETURNCODE=1
+    else
+        RETURNCODE=0
+        continue
+    fi
+
+    sleep 2
+done
+
+exit "${RETURNCODE}"
+
+""" % (daemon_string, timeout))
+
+class Puppet(object):
+
+    def __init__(self, configfile):
+        pass
+
+    @classmethod
+    def apply(cls, module_name, args_vector, metadata):
+
+        args_array = []
+        for key in args_vector:
+            args_array.append("%s => %s" % (key, args_vector[key]))
+
+        args_string = ", ".join(args_array)
+
+        if metadata.config['debug'] == True:
+            debug_flag = '--debug'
+        else:
+            debug_flag = ''
+
+        if metadata.config['verbose'] == True:
+            verbose_flag = '--verbose'
+        else:
+            verbose_flag = ''
+
+        if module_name == 'midonet::repository':
+            pass
+        else:
+            xargs = {}
+
+            if "OS_MIDOKURA_REPOSITORY_USER" in os.environ:
+                xapt = 'http://%s:%s@apt.midokura.com' % (os.environ["OS_MIDOKURA_REPOSITORY_USER"], os.environ["OS_MIDOKURA_REPOSITORY_PASS"])
+                xargs['midonet_stage'] = "'trusty'"
+                xargs['openstack_release'] = "'juno'"
+                xargs['midonet_repo'] = "'%s/midonet/v1.9/stable'" % xapt
+                xargs['midonet_openstack_repo'] = "'%s/openstack/juno/stable'" % xapt
+            else:
+                xapt = 'http://repo.midonet.org'
+                xargs['midonet_stage'] = "'stable'"
+                xargs['openstack_release'] = "'juno'"
+                xargs['midonet_repo'] = "'%s/midonet/v2014.11'" % xapt
+                xargs['midonet_openstack_repo'] = "'%s/openstack'" % xapt
+                xargs['midonet_thirdparty_repo'] = "'%s/misc'" % xapt
+                xargs['midonet_key'] = "'50F18FCF'"
+                xargs['midonet_key_url'] = "'%s/packages.midokura.key'" % xapt
+
+            repo_args = []
+
+            for key in xargs:
+                repo_args.append("%s => %s" % (key, xargs[key]))
+
+            run("""
+DEBUG="%s"
+VERBOSE="%s"
+
+echo 'nameserver 8.8.8.8' > /etc/resolv.conf
+yes | dpkg --configure -a
+
+cat >/tmp/node.pp<<EOF
+node $(hostname) {
+    class { 'midonet::repository':
+        %s
+    }
+    ->
+    class { '%s':
+        %s
+    }
+}
+EOF
+
+puppet apply ${DEBUG} ${VERBOSE} /tmp/node.pp 2>&1
+
+""" % (debug_flag, verbose_flag, ",".join(repo_args), module_name, args_string))
+
---
+++ stages/install/fabfile.py
@@ -0,0 +1,77 @@
+    puts(yellow("installing packages and configuring server [%s]" % env.host_string))
+
+    #
+    # prepare the box
+    #
+    run("""
+DOMAIN="%s"
+HOST="%s"
+
+UNDERLAY_MTU="%s"
+
+echo 'nameserver 8.8.8.8' > /etc/resolv.conf
+
+yes | dpkg --configure -a
+apt-get update 1>/dev/null
+DEBIAN_FRONTEND=noninteractive apt-get -y -u dist-upgrade 1>/dev/null
+
+for PKG in puppet htop vim screen atop tcpdump nload make git dstat bridge-utils openjdk-7-jre-headless iperf iperf3 traceroute mosh python minicom strace; do
+    dpkg -l "${PKG}" | grep "ii  ${PKG}" || \
+        DEBIAN_FRONTEND=noninteractive apt-get -q --yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install -- "${PKG}" || true
+done
+
+puppet module list 2>/dev/null | grep midonet-midonet || puppet module install midonet-midonet
+
+# patch the datastax key id to avoid the pesky warning
+sed -i "s|key        => 'B999A372',|key        => '7E41C00F85BFC1706C4FFFB3350200F2B999A372',|g;" /etc/puppet/modules/midonet/manifests/repository/ubuntu.pp
+
+mkdir -pv /etc/puppet
+touch /etc/puppet/hiera.yaml
+
+echo "${HOST}" > /etc/hostname
+
+hostname "${HOST}"
+
+cat >/etc/hosts<<EOF
+127.0.0.1 localhost.localdomain localhost
+127.0.0.1 $(hostname).${DOMAIN} $(hostname)
+
+# The following lines are desirable for IPv6 capable hosts
+::1 ip6-localhost ip6-loopback
+fe00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+ff02::3 ip6-allhosts
+EOF
+
+#
+# MTU
+#
+ip link set dev eth0 mtu ${UNDERLAY_MTU} || echo
+ip link set dev eth1 mtu ${UNDERLAY_MTU} || echo
+
+exit 0
+
+""" % (metadata.config["domain"], env.host_string, metadata.config["underlay_mtu"]))
+
+    #
+    # MEM
+    #
+    if "OS_MIDOKURA_REPOSITORY_USER" in os.environ:
+        user = os.environ["OS_MIDOKURA_REPOSITORY_USER"]
+        password = os.environ["OS_MIDOKURA_REPOSITORY_PASS"]
+
+        apt = "http://%s:%s@apt.midokura.com" % (user, password)
+
+        run("""
+APT="%s"
+
+KEY="${APT}/packages.midokura.key"
+
+wget -SO- "${KEY}" 2>/dev/null | /usr/bin/apt-key add -
+
+wget -SO- "${KEY}" 2>&1
+
+""" % apt)
+
